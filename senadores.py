484f2f1acfb3629975c77310629ed2300e3695ba
Tomas Rios <torios@frba.utn.edu.ar>
2025-12-10 12:55:05 -0300

    Add headless option to Selenium options

--- senadores.py
+++ senadores.py
@@ -14,6 +14,7 @@ class ScrapearSenado:
     def __init__(self):
         print("Inicializando robot Senado...")
         options = Options()
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--window-size=1920,1080')
